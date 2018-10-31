3c16adf0ebe40dcf3f8998e7ff84d390d934a012
VOLODYMYR <v.n.zubarev@gmail.com>
2018-10-31 16:20:48 +0200

    inspected with rubocop and made some small changes in syntax

--- post.rb
+++ post.rb
@@ -1,6 +1,6 @@
+require 'sqlite3'
+
 class Post
-
-
   # static class method
   def self.post_types
     [Memo, Link, Task]
@@ -8,7 +8,7 @@ class Post
 
   # static child ancestors creator
   def self.create(type_index)
-    return post_types[type_index].new
+    post_types[type_index].new
   end
 
   def initialize
@@ -16,26 +16,25 @@ class Post
     @text = nil
   end
 
-  def read_from_console
-  end
+  def read_from_console; end
 
-  def to_strings
-  end
+  def to_strings; end
 
   def save_in_file
     file = File.new(file_path, 'w:UTF-8')
 
-    for item in to_strings
-      file.puts(item)
-    end
+    # for item in to_strings
+    to_strings.each { |item| file.puts(item) }
     file.close
   end
 
   def file_path
+    n = self.class.name
+
     current_path = File.dirname(__FILE__)
 
-    file_name = @created_at.strftime("#{self.class.name}" + "/" + "#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    file_name = @created_at.strftime(n.to_s + '/' + "#{n}_%Y-%m-%d_%H-%M-%S.txt")
 
-    current_path + "/" + file_name
+    current_path + '/' + file_name
   end
 end
